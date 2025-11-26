04376c1232afa7cca66e6d8971ac5073608c9c6b
narinemarutyan <narine_marutyan@epam.com>
2025-11-26 20:55:51 +0400

    add knowledge

--- src/router.py
+++
@@ -1,9 +0,0 @@
-from src.config import API_KEY
-from langchain_openai import ChatOpenAI
-from langchain.agents import create_agent
-from langchain.tools import BaseTool
-
-
-
-result = agent.invoke({"input": "reverse hello"})
-print(result)
